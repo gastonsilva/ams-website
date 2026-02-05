8f95b7098555c343abe2cba29217cb8ad2e98db8
Gaston Silva <gaston.silva@gmail.com>
2026-02-05 19:58:05 -0300

    feat: implement Astro View Transitions, loading indicator, and performance measurement

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
   server: { port: 6789, host: true, allowedHosts: ["www.ams.lan"] },
   vite: {
     css: {
